a83df96bc858f240172c27aa14e23229b2373444
Shayancx <Shayan.Ashtarany@protonmail.com>
2025-08-04 14:15:52 +0200

    Refactor CLI to use OptionParser

--- lib/ebook_reader/cli.rb
+++ lib/ebook_reader/cli.rb
@@ -1,15 +1,41 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 module EbookReader
   # The command-line interface for the Ebook Reader application.
   class CLI
-    def self.run
-      debug = ARGV.include?('--debug') || ENV.fetch('DEBUG', nil)
+    class << self
+      def run(argv = ARGV)
+        options, args = parse_options(argv)
+        setup_logger(options[:debug] || ENV.fetch('DEBUG', nil))
 
-      Infrastructure::Logger.output = debug ? $stdout : File.open(File::NULL, 'w')
-      Infrastructure::Logger.level = debug ? :debug : :error
+        if args.first
+          Reader.new(args.first).run
+        else
+          MainMenu.new.run
+        end
+      end
 
-      MainMenu.new.run
+      private
+
+      def parse_options(argv)
+        options = { debug: false }
+        parser = OptionParser.new
+        parser.banner = 'Usage: ebook_reader [options] [file]'
+        parser.on('-d', '--debug', 'Enable debug logging') { options[:debug] = true }
+        parser.on('-h', '--help', 'Prints this help') do
+          puts parser
+          exit
+        end
+        parser.parse!(argv)
+        [options, argv]
+      end
+
+      def setup_logger(debug)
+        Infrastructure::Logger.output = debug ? $stdout : File.open(File::NULL, 'w')
+        Infrastructure::Logger.level = debug ? :debug : :error
+      end
     end
   end
 end
